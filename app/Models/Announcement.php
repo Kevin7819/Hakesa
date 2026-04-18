d91ddfb53b672bee17c81461736da4fbc859c349
Kevin7819 <kevin7819@gmail.com>
2026-04-18 10:41:29 -0600

    style: fix Pint - fully_qualified_strict_types and ordered_imports
    The CI runs with the latest Pint which has stricter rules:
    - Use Carbon at top level (not prefixed in docblock)
    - Alphabetically sorted imports

--- app/Models/Announcement.php
+++ app/Models/Announcement.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Builder;
@@ -16,13 +17,13 @@ use Illuminate\Support\Facades\Date;
  * @property string $title
  * @property string $description
  * @property string|null $image
- * @property \Carbon\Carbon $event_date
+ * @property Carbon $event_date
  * @property string|null $location
  * @property string|null $link
  * @property bool $is_active
- * @property \Carbon\Carbon|null $expires_at
- * @property \Carbon\Carbon $created_at
- * @property \Carbon\Carbon $updated_at
+ * @property Carbon|null $expires_at
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
  */
 #[Fillable(['title', 'description', 'image', 'event_date', 'location', 'link', 'is_active', 'expires_at'])]
 #[Hidden(['created_at', 'updated_at'])]
